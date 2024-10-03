375108fdb209f2262e0f2b242071fdf3d305b91f
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-10-04 00:09:32 +0400

    built phone version, closer to finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Post from './components/post/post'
+import Mark from '/images/avatar-mark-webber.webp'
+import Angela from '/images/avatar-angela-gray.webp'
+import Jacob from '/images/avatar-jacob-thompson.webp'
+import Rizky from '/images/avatar-rizky-hasanuddin.webp'
+import Kimberly from '/images/avatar-kimberly-smith.webp'
+import Nathan from '/images/avatar-nathan-peterson.webp'
+import Anna from '/images/avatar-anna-kim.webp'
+import Chess from '/images/image-chess.webp'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let notifications = [
+    {
+      id: 1,
+      name: "Mark Webber",
+      action: "reacted to your recent post",
+      post: "My first tournament today!",
+      group: "",
+      date: "1m ago",
+      avatar: Mark,
+      message: "",
+      picture: null,
+      read: false
+    },
+    {
+      id: 2,
+      name: "Angela Gray",
+      action: "followed you",
+      post: "",
+      group: "",
+      date: "5m ago",
+      avatar: Angela,
+      message: "",
+      picture: null,
+      read: false
+    },
+    {
+      id: 3,
+      name: "Jacob Thompson",
+      action: "has joined your group",
+      post: "",
+      group: "Chess Club",
+      date: "1 day ago",
+      avatar: Jacob,
+      message: "",
+      picture: null,
+      read: false
+    },
+    {
+      id: 4,
+      name: "Rizky Hasanuddin",
+      action: "sent you private message",
+      post: "",
+      group: "",
+      date: "5 days ago",
+      avatar: Rizky,
+      message: "Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.",
+      picture: null,
+      read: true
+    },
+    {
+      id: 5,
+      name: "Kimberly Smith",
+      action: "commented on your picture",
+      post: "",
+      group: "",
+      date: "1 week ago",
+      avatar: Kimberly,
+      message: "",
+      picture: Chess,
+      read: true
+    },
+    {
+      id: 6,
+      name: "Nathan Peterson",
+      action: "reacted to your recent post",
+      post: "5 end-game strategies to increase your win rate",
+      group: "",
+      date: "2 weeks ago",
+      avatar: Nathan,
+      message: "",
+      picture: null,
+      read: true
+    },
+    {
+      id: 7,
+      name: "Anna Kim",
+      action: "left the group",
+      post: "",
+      group: "Chess Club",
+      date: "2 weeks ago",
+      avatar: Anna,
+      message: "",
+      picture: null,
+      read: true
+    }
+  ]
 
   return (
     <>
+      <main>
+        <div className="notif-header">
+          <div className="notification-numb">
+            <h1 className='notification-font'>Notifications</h1>
+            <div className="Nnumb">
+              <h1 id='notif-number'>3</h1>
+            </div>
+          </div>
+          <h1 id='mark-all'>Mark all as read</h1>
+        </div>
+        {notifications.map((post) => (
+          <Post key={post.id} obj={post}/>
+        ))}
+      </main>
     </>
   )
 }
---
+++ src/components/post/post.jsx
@@ -0,0 +1,21 @@
+import './post.css'
+
+export default function post({obj}){
+    return (
+        <div className="post-div" key={obj.id} style={{backgroundColor: obj.read ? "none" : "#F7FAFD"}}>
+            <img src={obj.avatar} alt="avatar"/>
+            <div className="main-part">
+                <div className="action-message-div">
+                    <div className="action-message">
+                        <h1 className='main-message'><span className='name'>{obj.name}</span>{obj.action} 
+                        <span className='post'>{obj.post}</span><span className='group'>{obj.group}</span>
+                        {obj.read ? "" : <span className='red-circle'>🔴</span>}</h1>
+                        {obj.picture ? <img src={obj.picture} alt="picture" /> : ""}
+                    </div>
+                </div>
+                <h1 className='date'>{obj.date}</h1>
+                {obj.message ? <div className='direct-message'><h1>{obj.message}</h1></div> : ""}
+            </div>
+        </div>
+    )
+}
